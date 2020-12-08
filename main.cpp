f8bc41dd3f88ff43eb823ea6b0f9ec78492fa73f
ameliemelorene <72452572+ameliemelorene@users.noreply.github.com>
2020-12-08 16:40:35 +0100

    Update main.cpp
    ajout en commentaire des résultats du test1

--- main.cpp
+++ main.cpp
@@ -3,22 +3,22 @@
 
 void test1(){//test du cas normal
     IntStack pile (4);
-    pile.AffichePile();
+    pile.AffichePile();//[]
     bool empty=pile.vide();
-    std::cout<<empty<<std::endl;
+    std::cout<<empty<<std::endl;//1
     pile.push(1);
-    pile.AffichePile();
+    pile.AffichePile();//[1,]
     pile.push(2);
-    pile.AffichePile();
+    pile.AffichePile();//[1,2,]
     pile.push(3);
-    pile.AffichePile();
+    pile.AffichePile();//[1,2,3,]
     pile.push(4);
-    pile.AffichePile();
-    bool test = pile.rempli();
+    pile.AffichePile();[1,2,3,4,]
+    bool test = pile.rempli();//
     int r=pile.pop();
-    pile.Affichepile();
-    std::cout<<test<<std::endl;
-    std::cout<<r<<std::endl;
+    pile.Affichepile();//[1,2,3,]
+    std::cout<<test<<std::endl;//1
+    std::cout<<r<<std::endl;//4
     pile.delete_stack();
 }
 
