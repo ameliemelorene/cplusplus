97dd392513408b77aa336fb3a2462e81d04b7111
ameliemelorene <72452572+ameliemelorene@users.noreply.github.com>
2020-12-08 16:22:35 +0100

    Update main.cpp
    ajout du test "cas normal"

--- main.cpp
+++ main.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
-#include "instack.h"
+#include "intstack.h"
+
+void test1(){//test du cas normal
+    IntStack pile (4);
+    pile.AffichePile();
+    bool empty=pile.vide();
+    std::cout<<empty<<std::endl;
+    pile.push(1);
+    pile.AffichePile();
+    pile.push(2);
+    pile.AffichePile();
+    pile.push(3);
+    pile.AffichePile();
+    pile.push(4);
+    pile.AffichePile();
+    bool test = pile.rempli();
+    int r=pile.pop();
+    std::cout<<test<<std::endl;
+    std::cout<<r<<std::endl;
+    pile.delete_stack();
+}
+
 
 int main(){
-
-    IntStack pile (10);
-    pile.pop;
-    pile.push(1);
-    pile.push(4);
-
+    test1();
     return 0;
 }
